7669c07a6717ee4613faf69a1682df0a906fbfb6
wangyuanbo <834187444@qq.com>
2019-06-29 18:12:02 +0800

    上传test

---
+++ src/main/java/com/itcast/test.java
@@ -0,0 +1,28 @@
+package com.itcast;
+
+/**
+
+* @name  admin
+* @create 2019-06-29 18:02
+* 
+
+**/
+
+
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+
+
+    }
+
+}
