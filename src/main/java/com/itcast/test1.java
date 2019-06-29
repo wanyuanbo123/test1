f1cadd7385613d49684c5bb11151db15419f76ea
wangyuanbo <834187444@qq.com>
2019-06-29 18:36:20 +0800

    上传test

---
+++ src/main/java/com/itcast/test1.java
@@ -0,0 +1,17 @@
+package com.itcast;
+
+/**
+
+* @name  admin
+* @create 2019-06-29 18:29
+* 
+
+**/
+
+
+public class test1 {
+
+    public void test1(){
+        System.out.println("123");
+    }
+}
